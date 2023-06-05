8bda109d410022da404621ededdd76e8b04f9921
Letov Sergey <letovs87@gmail.com>
2023-06-05 16:24:18 +0300

    Апдэйт VM главного окна

--- StudyWPFProject/ViewModels/MainWindowViewModel.cs
+++ StudyWPFProject/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using StudyWPFProject.Models;
+using StudyWPFProject.Services;
 using StudyWPFProject.ViewModels.Base;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows.Documents;
 
 namespace StudyWPFProject.ViewModels
@@ -10,18 +13,50 @@ namespace StudyWPFProject.ViewModels
         private string _Title = "My app";
         public string Title { get => _Title; set => Set(ref _Title, value); }
 
-        private ObservableCollection<Teacher> _Teachers = new ObservableCollection<Teacher>();
-        public ObservableCollection<Teacher> Teachers { get => _Teachers; set => Set(ref _Teachers, value); }
 
-        
-        public MainWindowViewModel()
+        private Teacher _SelectedTeacher;
+        public Teacher SelectedTeacher { get => _SelectedTeacher; set => Set(ref _SelectedTeacher, value); }
+
+        private ObservableCollection<Teacher> Teachers { get; set; } = new ObservableCollection<Teacher>();
+
+        public List<Institute> Institutes = new List<Institute>()
         {
-            Teachers = new ObservableCollection<Teacher>()
-            {
-                new Teacher(){FullName = "Pupkin", Service = new Service(){Name = "Skype"},Institute = new Institute(){ Name = "ITMO"} },
-                new Teacher(){FullName = "Vasay", Service = new Service(){Name = "Skype"}, Institute = new Institute(){ Name = "BGTU"} }
-            };
+            new Institute(){Name = "ИКИТ"},
+            new Institute(){Name = "ГИ"},
+            new Institute(){Name = "ИППС"},
+            new Institute(){Name = "ИУБП"}
+        };
+        public List<Service> Services = new List<Service>()
+        {
+            new Service(){Name = "Discord"},
+            new Service(){Name = "Zoom"},
+            new Service(){Name = "WebinarSFU"}
+        };
+
+        private List<TopServiceItem> _TopServices;
+        public List<TopServiceItem>  TopServices { get => _TopServices; set => Set(ref _TopServices, value); }
+        public Teacher NewTeacher { get; set; } = new Teacher();
+        private void GetTop3Services()
+        {
+            TopServices = new List<TopServiceItem>(
+                Teachers.GroupBy(p => p.Service)
+                        .Select
+                            (p => new TopServiceItem
+                                {
+                                    ServiceName = p.Key.Name,
+                                    CountOfUsing = p.Count()
+                                }
+                            )
+                        .OrderByDescending(t => t.CountOfUsing)
+                        .Take(3)
+                        .ToList());
         }
 
+        public MainWindowViewModel()
+        {
+            Teachers.Add(new Teacher() { FullName = "Sergey", Institute = new Institute() { Name = "ГИ" }, Service = new Service() { Name = "Discord" } });
+        }
     }
+
+    
 }
