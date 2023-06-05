ca9277a0e0e8ab30edfcb90737dacd09533bec00
Sergey Letov <letovs87@gmail.com>
2023-06-05 11:49:17 +0300

    Настроен DI, IoC для работы приложения.

--- StudyWPFProject/App.xaml.cs
+++ StudyWPFProject/App.xaml.cs
@@ -1,17 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.Windows;
 
 namespace StudyWPFProject
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+    
     public partial class App : Application
     {
+        public static IHost? AppHost { get; private set; }
+        public App()
+        {
+            AppHost = Host.CreateDefaultBuilder().ConfigureServices((host, services) =>
+            {
+                services.AddSingleton<MainWindow>();
+            }).Build();
+        }
+        protected  override async void OnStartup(StartupEventArgs e)
+        {
+            await AppHost!.StartAsync();
+            var startApp = AppHost!.Services.GetRequiredService<MainWindow>();
+            startApp.Show();
+            base.OnStartup(e);
+        }
+        protected  override async void OnExit(ExitEventArgs e)
+        {
+            await AppHost!.StopAsync();
+            base.OnExit(e); 
+        }
     }
 }
