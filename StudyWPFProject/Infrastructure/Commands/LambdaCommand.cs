553cb5f18885b0da50aa0d9c3a86edcd8c04868a
Sergey Letov <letovs87@gmail.com>
2023-06-05 18:26:03 +0300

    Добавлена рабочая команда

---
+++ StudyWPFProject/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,26 @@
+﻿
+using StudyWPFProject.Infrastructure.Commands.Base;
+using System;
+
+namespace StudyWPFProject.Infrastructure.Commands;
+
+internal class LambdaCommand : BaseCommand
+{
+    private readonly Action<object> _Execute;
+    private readonly Func<object, bool> _CanExecute;
+
+    public override bool CanExecute(object? parameter)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Execute(object? parameter)
+    {
+        throw new System.NotImplementedException();
+    }
+    public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null!)
+    {
+        _Execute = Execute;
+        _CanExecute = CanExecute;
+    }
+}
