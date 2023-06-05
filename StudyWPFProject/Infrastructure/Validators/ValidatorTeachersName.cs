de5d14ab67d3ba73a710e38736cedc23022105ed
Sergey Letov <letovs87@gmail.com>
2023-06-05 21:42:02 +0300

    Реализован, без проверки валидатор данных

--- StudyWPFProject/Infrastructure/Validators/ValidatorTeachersName.cs
+++ StudyWPFProject/Infrastructure/Validators/ValidatorTeachersName.cs
@@ -13,7 +13,27 @@ namespace StudyWPFProject.Infrastructure.Validators
         public string? InputString { get; set; }
         public override ValidationResult Validate(object value, CultureInfo cultureInfo)
         {
-            throw new NotImplementedException();
+            if (value == null) return null!;
+
+            if (value is not string inputString || string.IsNullOrWhiteSpace(inputString)) return null!;
+
+            var result = inputString.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            try
+            {
+                var res = result.Length switch
+                {
+                    1 => result[0],
+                    2 => string.Format("{0} {1}.", result[0], result[1]),
+                    3 => string.Format("{0} {1} {2}.", result[0], result[1], result[2]),
+                    _ => throw new ArgumentException("Too many whitespaces in input string.")
+                };
+                return ValidationResult.ValidResult;
+            }
+            catch (Exception)
+            {
+                return new ValidationResult(false, "Please enter correct data.");
+            }
             //TODO доделать валидатор
         }
     }
