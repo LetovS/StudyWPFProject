d8a84e62b9eb780dcf8e0e1d5b8d508832593a06
Letov Sergey <letovs87@gmail.com>
2023-06-05 15:00:26 +0300

    Добавлено базовое представление VM модели

---
+++ StudyWPFProject/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyWPFProject.ViewModels.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public virtual void OnPropertyChanged([CallerMemberName] string propName = null!)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string propName = null!)
+        {
+            if(Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propName);
+            return true;
+        }
+        
+    }
+}
