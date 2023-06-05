3c2809dea355fd9e4c0b9a97fd4e39177ca4606d
Letov Sergey <letovs87@gmail.com>
2023-06-05 15:00:55 +0300

    Добавлено VM модель главного представления

---
+++ StudyWPFProject/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using StudyWPFProject.Models;
+using StudyWPFProject.ViewModels.Base;
+using System.Collections.ObjectModel;
+using System.Windows.Documents;
+
+namespace StudyWPFProject.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "My app";
+        public string Title { get => _Title; set => Set(ref _Title, value); }
+
+        private ObservableCollection<Teacher> _Teachers = new ObservableCollection<Teacher>();
+        public ObservableCollection<Teacher> Teachers { get => _Teachers; set => Set(ref _Teachers, value); }
+
+        
+        public MainWindowViewModel()
+        {
+            Teachers = new ObservableCollection<Teacher>()
+            {
+                new Teacher(){FullName = "Pupkin", Service = new Service(){Name = "Skype"},Institute = new Institute(){ Name = "ITMO"} },
+                new Teacher(){FullName = "Vasay", Service = new Service(){Name = "Skype"}, Institute = new Institute(){ Name = "BGTU"} }
+            };
+        }
+
+    }
+}
